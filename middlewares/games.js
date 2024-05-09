00729bf3ecdfba1d841a501edac6232a64efbb6a
Юлия Тюрина <juliosoctober@gmail.com>
2024-05-09 18:56:00 +0300

    initial

---
+++ middlewares/games.js
@@ -0,0 +1,21 @@
+const { readData } = require("../utils/data"); // Чтение и запись данных в JSON-файл
+
+
+// Получим игры из JSON-файла
+const getAllGames = async (req, res, next) => {
+    const games = await readData("./utils/data/games.json");
+    if (!games) {
+      res.status(400);
+      res.send({
+        status: "error",
+        message: "Нет игр в базе данных. Добавьте игру."
+      });
+      return;
+    }
+    req.games = games;
+    next()
+  };
+
+  module.exports = {
+    getAllGames
+  }
